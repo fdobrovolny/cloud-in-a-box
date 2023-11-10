3c3bbe0b5277ca8dfdd76a9a499af74f17913840
Marc Schöchlin <schoechlin@osb-alliance.com>
2023-11-10 19:34:31 +0100

    bugfix

--- init_ciab.sh
+++ init_ciab.sh
@@ -16,6 +16,6 @@ fi
 
 set -xe
 git clone "${ciab_repo_url:-https://github.com/osism/cloud-in-a-box}" /opt/cloud-in-a-box
-git checkout -C /opt/cloud-in-a-box "${ciab_branch:-main}"
+git -C /opt/cloud-in-a-box checkout "${ciab_branch:-main}"
 /opt/cloud-in-a-box/bootstrap.sh sandbox
 /opt/cloud-in-a-box/deploy.sh sandbox
